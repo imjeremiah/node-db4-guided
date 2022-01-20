60021cad097fdb4db7eba0bbddae3d24cf386349
Ladrillo <gabriel@cabrejas.net>
2022-01-20 19:21:55 +0100

    committing to "lecture"

--- data/migrations/20220120180742_first-migration.js
+++ data/migrations/20220120180742_first-migration.js
@@ -12,6 +12,7 @@ exports.up = async function (knex) {
     .createTable('animals', table => {
       table.increments('animal_id')
       table.string('animal_name', 100).notNullable()
+      table.integer('')
     })
     .createTable('zoo_animals', table => {
       table.increments('zoo_animal_id')
