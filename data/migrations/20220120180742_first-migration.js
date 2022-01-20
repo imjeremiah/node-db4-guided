94e3adf2822328a21522632b6dde30c78c7205aa
Ladrillo <gabriel@cabrejas.net>
2022-01-20 19:12:39 +0100

    committing to "lecture"

--- data/migrations/20220120180742_first-migration.js
+++ data/migrations/20220120180742_first-migration.js
@@ -1,16 +1,16 @@
 
 exports.up = async function (knex) {
   await knex.schema
-    .createTable('zoos')
-    .createTable('species')
-    .createTable('animals')
-    .createTable('zoo_animals')
+    .createTable('zoos', )
+    .createTable('species', )
+    .createTable('animals', )
+    .createTable('zoo_animals', )
 };
 
 exports.down = async function (knex) {
   await knex.schema
-    .dropTableIfExists('zoo_animal')
-    .dropTableIfExists()
-    .dropTableIfExists()
-    .dropTableIfExists()
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('species')
+    .dropTableIfExists('zoos')
 };
