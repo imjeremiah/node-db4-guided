48be5d558287e6164b9bf151d0c4d4b986356ed9
Ladrillo <gabriel@cabrejas.net>
2022-01-20 19:09:21 +0100

    committing to "lecture"

--- data/migrations/20220120180742_first-migration.js
+++ data/migrations/20220120180742_first-migration.js
@@ -9,4 +9,8 @@ exports.up = async function (knex) {
 
 exports.down = async function (knex) {
   await knex.schema
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
 };
